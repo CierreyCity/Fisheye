c818348bc7afbceb97f9366c44c95ebff74cd2ab
CierreyCity <bornaisfabien@gmail.com>
2026-02-08 23:48:36 +0100

    lightbox

---
+++ utils/lightbox.js
@@ -0,0 +1,10 @@
+let currentMedia = 0;
+const lightboxContainer = document.getElementById("lightboxContainer");
+
+function openLightbox(index) {
+  const allMedias = document.getElementsByClassName("media-card");
+  const type = allMedias[index].tagName;
+
+  const src = allMedias[index].getAttribute("src");
+  let fileName = src.split("/").pop().split(".")[0];
+  fileName = fileName.replace(/_/g, " ");
